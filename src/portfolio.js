270994dbd447d73e6f68401dd6d52e5ee773e79a
lynx479 <157099354+lynx479@users.noreply.github.com>
2025-05-05 23:25:50 +0530

    Your commit message here

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,10 +20,10 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Sauud",
-  title: "Hi all, I'm messi",
+  username: "Saud Abdul Latheef",
+  title: "Hi all, Saud Abdul Latheef",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A passionate Full Stack Software Develop 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
