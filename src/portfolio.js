c7cec0e4348181d6d2556f1043bef4165df4262d
lynx479 <saudabdullatheef@gmail.com>
2025-05-18 17:37:22 +0300

    contact details

--- src/portfolio.js
+++ src/portfolio.js
@@ -372,8 +372,8 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+  number: "+91 7902995052",
+  email_address: "saudabdullatheef@gmail.com"
 };
 
 // Twitter Section
